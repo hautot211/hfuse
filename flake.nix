8583b100c9bc62a5101d78085c33e2bc4a925a21
hautot211 <hautot211@unicaen.fr>
2025-05-17 15:40:51 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "flake-utils";
+    hfsutils.url = "git+file:///home/guillaume/Documents/fuse-tests/hfsutils?ref=nix";
+  };
+
+  outputs = {self, nixpkgs, flake-utils, hfsutils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = [
+          hfsutils
+        ];
+        buildInputs = [
+          pkgs.gnumake
+          pkgs.cmake
+          pkgs.fuse3
+        ];
+        hfuse = pkgs.stdenv.mkDerivation {
+          pname = "hfuse";
+          version = "0.1.0";
+          src = self;
+          inherit buildInputs nativeBuildInputs;
+        };
+      in
+    {
+      packages.default = hfuse;
+      devShells.default = pkgs.mkShell {
+        inherit buildInputs nativeBuildInputs;
+      };
+    }
+  );
+}
