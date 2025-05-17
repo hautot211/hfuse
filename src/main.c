eb8a15eede1a50077f762bb472c2c33db0b194f7
hautot211 <hautot211@unicaen.fr>
2025-05-17 17:14:24 +0200

    added hello.c fuse template

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <hfs.h>
-#include <fuse.h>
+#define FUSE_USE_VERSION 35
+#include <fuse3/fuse.h>
+
 
 int main() {
     hfsvol* volume = hfs_mount("/home/guillaume/Documents/fuse-test/samples/MacOS-9.0.4-fresh.rom", 0, HFS_MODE_RDONLY);
-    printf("hfsvol ref : %lld\n", volume);
-    printf("hello\n");
+
+    hfsvolent* volume_entity = (hfsvolent*) malloc(sizeof(hfsvolent));
+    hfs_vstat(volume, volume_entity);
+    printf("%s\n", volume_entity->name);
+    free(volume_entity);
+
+    hfs_umountall();
 }
